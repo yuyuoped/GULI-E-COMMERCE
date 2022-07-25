85f558f0bd84eda24ed752e7e83792aec431ee97
yuyuoped <yuyuoped999@gmail.com>
2022-07-25 23:11:25 +0800

    将oss accesskey设置为本地保存

--- gmall-pms/src/main/java/com/yuyuoped/gmall/pms/config/OssConfig.java
+++ gmall-pms/src/main/java/com/yuyuoped/gmall/pms/config/OssConfig.java
@@ -1,6 +1,7 @@
 package com.yuyuoped.gmall.pms.config;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -10,7 +11,15 @@ import org.springframework.context.annotation.PropertySource;
  */
 @Configuration
 @Data
-@PropertySource("classpath:application-local.yml")
+//使用yml需要引入额外依赖
+@PropertySource("classpath:application-local.properties")
 public class OssConfig {
-
+    @Value("${aliyun.oss.endpoint}")
+    private String endpoint;
+    @Value("${aliyun.oss.accessKeyId}")
+    private String accessKeyId;
+    @Value("${aliyun.oss.accessKeySecret}")
+    private String accessKeySecret;
+    @Value("${aliyun.oss.bucketName}")
+    private String bucketName;
 }
--- gmall-pms/src/main/java/com/yuyuoped/gmall/pms/controller/OssController.java
+++ gmall-pms/src/main/java/com/yuyuoped/gmall/pms/controller/OssController.java
@@ -6,6 +6,8 @@ import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
 import com.yuyuoped.gmall.common.bean.ResponseVo;
+import com.yuyuoped.gmall.pms.config.OssConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,17 +25,20 @@ import java.util.Map;
 @RequestMapping("/pms/oss")
 public class OssController {
 
+    @Autowired
+    OssConfig ossConfig;
+
     @GetMapping("/policy")
     public ResponseVo<Object> policy() {
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessId = "LTAI5tCxDjyjUn513g9HubZe";
-        String accessKey = "w3Ki3GEBeRHw3SPzv12lVGkYVdpH1q";
+        String accessId = ossConfig.getAccessKeyId();
+        String accessKey = ossConfig.getAccessKeySecret();
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-        String endpoint = "oss-cn-shanghai.aliyuncs.com";
+        String endpoint = ossConfig.getEndpoint();
         // 填写Bucket名称，例如examplebucket。
-        String bucket = "guli-gamll";
+        String bucket = ossConfig.getBucketName();
         // 填写Host地址，格式为https://bucketname.endpoint。
-        String host = "https://guli-gamll.oss-cn-shanghai.aliyuncs.com";
+        String host = "https://" + bucket + "." + endpoint;
 //        // 设置上传回调URL，即回调服务器地址，用于处理应用服务器与OSS之间的通信。OSS会在文件上传完成后，把文件上传信息通过此回调URL发送给应用服务器。
 //        String callbackUrl = "https://192.168.0.0:8888";
         // 设置上传到OSS文件的前缀，可置空此项。置空后，文件将上传至Bucket的根目录下。
