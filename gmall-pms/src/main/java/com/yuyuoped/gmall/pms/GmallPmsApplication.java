507d096c11235e40504bcd9b11d5aba1a66a0826
yuyuoped <yuyuoped999@gmail.com>
2022-07-29 00:50:39 +0800

    解决报错

--- gmall-pms/src/main/java/com/yuyuoped/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/yuyuoped/gmall/pms/GmallPmsApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableSwagger2
+@EnableFeignClients
 @MapperScan("com.yuyuoped.gmall.pms.mapper")
 public class GmallPmsApplication {
 
