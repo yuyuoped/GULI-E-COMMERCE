a076a652da88ccc544d8c219de29f0b8f548b632
yuyuoped <yuyuoped999@gmail.com>
2022-08-20 21:50:17 +0800

    基于Redisson的分布式锁和布隆过滤器解决首页数据缓存击穿和缓存穿透问题

---
+++ gmall-index/src/test/java/com/yuyuoped/gmall/index/GmallIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyuoped.gmall.index;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
